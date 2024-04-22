415369b9069e23f2259245054a6d2f35f22401cf
Denis Espanhol <denisgfespanhol@gmail.com>
2024-04-22 10:13:39 -0300

    Creating a component to showMoreButton

--- ts/src/app/app.module.ts
+++ ts/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ShowMoreButtonComponent } from './components/show-more-button/show-more-button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShowMoreButtonComponent
   ],
   imports: [
     BrowserModule
