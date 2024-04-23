b32c71f31e63f8fc8df95ef6c77d384a9015f869
Denis Espanhol <denisgfespanhol@gmail.com>
2024-04-23 10:40:47 -0300

    hidden create topic area when its clicked

--- ts/src/app/app.component.ts
+++ ts/src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  createTopicWasClicked: boolean = false;
+  createTopicWasClicked: boolean = true;
   visible: boolean = false;
 
   onclick()
